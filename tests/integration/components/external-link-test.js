cf98b2c330db7a2f80cc8aef34fbba1c11fc0cab
Charles Fries <charliefries@icloud.com>
2022-09-28 18:10:44 -0700

    Development

---
+++ tests/integration/components/external-link-test.js
@@ -0,0 +1,27 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'brokers/tests/helpers';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+// interface Context extends TestContext {
+//   url: string;
+// }
+
+module('Integration | Component | external-link', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function (assert) {
+    this.url = 'https://www.google.com/';
+
+    await render(hbs`
+      <ExternalLink @url={{this.url}}>
+        template block text
+      </ExternalLink>
+    `);
+
+    assert.dom('a').hasAttribute('href', this.url);
+    assert.dom('a').hasAttribute('target', '_blank');
+    assert.dom('a').hasAttribute('rel', 'noopener noreferrer');
+    assert.dom('a').hasText('template block text');
+  });
+});
