e8e1157b15f239120b8cb4dd811062d76ad469fa
Charles Fries <charliefries@icloud.com>
2022-10-20 15:45:31 -0700

    Development

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -1,5 +1,9 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
 
 export default class ApplicationController extends Controller {
   date = new Date();
+
+  @action
+  do() {}
 }
