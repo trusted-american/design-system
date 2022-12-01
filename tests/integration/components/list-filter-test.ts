8e2c429270cc2b4240a1252f3c0f52e518d8193f
Charles Fries <charliefries@icloud.com>
2022-12-01 13:56:17 -0800

    Generate list-filter component

---
+++ tests/integration/components/list-filter-test.ts
@@ -0,0 +1,55 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'dummy/tests/helpers';
+import { render, click, type TestContext } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+import type {
+  ListFilterSignature,
+  Predicate,
+} from '@trusted-american/design-system/components/list-filter';
+
+type Context = ListFilterSignature<string>['Args'] & TestContext;
+
+module('Integration | Component | list-filter', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function (this: Context, assert) {
+    assert.expect(3);
+
+    this.predicates = [
+      {
+        name: 'Created Date',
+        key: 'createdAt',
+        type: 'date',
+        value: [],
+      },
+      {
+        name: 'Status',
+        key: 'status',
+        value: undefined,
+        options: [
+          { value: 'one', label: 'One' },
+          { value: 'two', label: 'Two' },
+          { value: 'three', label: 'Three' },
+        ],
+      },
+    ] as Predicate<string>[];
+    this.onChange = () => {
+      assert.true(true);
+    };
+
+    await render(hbs`
+      <ListFilter
+        @predicates={{this.predicates}}
+        @onChange={{this.onChange}}
+      />
+    `);
+
+    assert.dom('[data-test-list-filter]').hasText('Filter');
+    assert.dom('[data-test-predicate-toggle]').exists({ count: 2 });
+
+    await click('[data-test-predicate-toggle] input');
+
+    assert.dom('[data-test-predicate-value]').exists({ count: 1 });
+  });
+});
