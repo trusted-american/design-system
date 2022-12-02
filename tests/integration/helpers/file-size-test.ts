bba055dd3929cf4670fe5bf1283b1243b34db499
Charles Fries <charliefries@icloud.com>
2022-12-02 10:01:36 -0800

    Cleanup

--- tests/integration/helpers/file-size-test.ts
+++ tests/integration/helpers/file-size-test.ts
@@ -1,17 +1,21 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'dummy/tests/helpers';
-import { render } from '@ember/test-helpers';
+import { render, type TestContext } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
+
+interface Context extends TestContext {
+  inputValue: number;
+}
 
 module('Integration | Helper | file-size', function (hooks) {
   setupRenderingTest(hooks);
 
   // TODO: Replace this with your real tests.
-  test('it renders', async function (assert) {
+  test('it renders', async function (this: Context, assert) {
     this.inputValue = 1234;
 
     await render(hbs`{{file-size this.inputValue}}`);
 
-    assert.dom(this.element).hasText('1.21 KB');
+    assert.dom(this.element as Element).hasText('1.21 KB');
   });
 });
