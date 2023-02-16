9699b0090e5122c6c6415a3808265177106f0730
Charles Fries <charliefries@icloud.com>
2023-02-16 14:56:32 -0800

    Cleanup

--- addon/template-registry.ts
+++ addon/template-registry.ts
@@ -1,36 +1,36 @@
-import type AsideComponent from '@trusted-american/design-system/components/aside';
-import type AvatarComponent from '@trusted-american/design-system/components/avatar';
-import type BreadcrumbTrailComponent from '@trusted-american/design-system/components/breadcrumb-trail';
-import type ExternalLinkComponent from '@trusted-american/design-system/components/external-link';
-import type HeadingComponent from '@trusted-american/design-system/components/heading';
-import type ListFilter from '@trusted-american/design-system/components/list-filter';
-import type MainComponent from '@trusted-american/design-system/components/main';
-import type PlaceholderComponent from '@trusted-american/design-system/components/placeholder';
-import type PropertyListComponent from '@trusted-american/design-system/components/property-list';
-import type StatCardComponent from '@trusted-american/design-system/components/stat-card';
-import type SubheadingComponent from '@trusted-american/design-system/components/subheading';
-import type ToastComponent from '@trusted-american/design-system/components/toast';
+import type AsideComponent from './components/aside';
+import type AvatarComponent from './components/avatar';
+import type BreadcrumbTrailComponent from './components/breadcrumb-trail';
+import type ExternalLinkComponent from './components/external-link';
+import type HeadingComponent from './components/heading';
+import type ListFilter from './components/list-filter';
+import type MainComponent from './components/main';
+import type PlaceholderComponent from './components/placeholder';
+import type PropertyListComponent from './components/property-list';
+import type StatCardComponent from './components/stat-card';
+import type SubheadingComponent from './components/subheading';
+import type ToastComponent from './components/toast';
 
-import type FormCheckComponent from '@trusted-american/design-system/components/form/check';
-import type FormDateInputComponent from '@trusted-american/design-system/components/form/date-input';
-import type FormFileInputComponent from '@trusted-american/design-system/components/form/file-input';
-import type FormInputComponent from '@trusted-american/design-system/components/form/input';
-import type FormPhoneInputComponent from '@trusted-american/design-system/components/form/phone-input';
-import type FormRadioBtnComponent from '@trusted-american/design-system/components/form/radio-btn';
-import type FormRadioComponent from '@trusted-american/design-system/components/form/radio';
-import type FormSelectComponent from '@trusted-american/design-system/components/form/select';
-import type FormTextareaComponent from '@trusted-american/design-system/components/form/textarea';
+import type FormCheckComponent from './components/form/check';
+import type FormDateInputComponent from './components/form/date-input';
+import type FormFileInputComponent from './components/form/file-input';
+import type FormInputComponent from './components/form/input';
+import type FormPhoneInputComponent from './components/form/phone-input';
+import type FormRadioBtnComponent from './components/form/radio-btn';
+import type FormRadioComponent from './components/form/radio';
+import type FormSelectComponent from './components/form/select';
+import type FormTextareaComponent from './components/form/textarea';
 
-import type MainBodyComponent from '@trusted-american/design-system/components/main/body';
-import type MainFooterComponent from '@trusted-american/design-system/components/main/footer';
-import type MainHeaderComponent from '@trusted-american/design-system/components/main/header';
-import type MainTopHeaderComponent from '@trusted-american/design-system/components/main/top-header';
+import type MainBodyComponent from './components/main/body';
+import type MainFooterComponent from './components/main/footer';
+import type MainHeaderComponent from './components/main/header';
+import type MainTopHeaderComponent from './components/main/top-header';
 
-import type PropertyListItemComponent from '@trusted-american/design-system/components/property-list/item';
+import type PropertyListItemComponent from './components/property-list/item';
 
-import type fileSize from '@trusted-american/design-system/helpers/file-size';
-import type fromNow from '@trusted-american/design-system/helpers/from-now';
-import type timestamp from '@trusted-american/design-system/helpers/timestamp';
+import type fileSize from './helpers/file-size';
+import type fromNow from './helpers/from-now';
+import type timestamp from './helpers/timestamp';
 
 export default interface Registry {
   Aside: typeof AsideComponent;
