bee3d90e661036c03aaa03b130005d6a1655611f
Charles Fries <charliefries@icloud.com>
2023-02-17 11:48:58 -0800

    Cleanup

--- tests/dummy/app/controllers/example.ts
+++ tests/dummy/app/controllers/example.ts
@@ -4,6 +4,12 @@ import { action } from '@ember/object';
 export default class ExampleController extends Controller {
   search = '';
 
+  users = [
+    { email: 'a@example.com', firstName: 'A', lastName: 'A' },
+    { email: 'b@example.com', firstName: 'B', lastName: 'B' },
+    { email: 'c@example.com', firstName: 'C', lastName: 'C' },
+  ];
+
   @action
   do() {
     console.log('Something');
