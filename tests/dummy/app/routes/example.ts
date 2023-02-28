6e24a1119e25fee37c75e216dbbbaba51ec1356b
Charles Fries <charliefries@icloud.com>
2023-02-27 18:59:21 -0800

    Loading

--- tests/dummy/app/routes/example.ts
+++ tests/dummy/app/routes/example.ts
@@ -1,3 +1,11 @@
 import Route from '@ember/routing/route';
 
-export default class ExampleRoute extends Route {}
+export default class ExampleRoute extends Route {
+  model() {
+    return new Promise<void>((resolve) => {
+      setTimeout(() => {
+        resolve();
+      }, 500);
+    });
+  }
+}
