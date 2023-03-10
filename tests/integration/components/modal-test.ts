6e2880947d3b7bfde03b589fc2f7744d3a36999d
Charles Fries <charliefries@icloud.com>
2023-03-10 13:26:17 -0800

    Fix tests

--- tests/integration/components/modal-test.ts
+++ tests/integration/components/modal-test.ts
@@ -8,11 +8,12 @@ module('Integration | Component | modal', function (hooks) {
 
   test('it renders', async function (assert) {
     await render(hbs`
+      {{! @glint-ignore}}
       <Modal @title="Title" @onClose={{fn (mut this.xyz)}}>
         template block text
       </Modal>
     `);
 
-    assert.dom().hasText('template block text');
+    assert.dom().hasText('Title template block text');
   });
 });
