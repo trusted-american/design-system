1a50cdf025406baef45d3a752d32e740fd246970
Charles Fries <charliefries@icloud.com>
2023-05-07 15:25:55 -0700

    Type initializer test

--- tests/unit/initializers/body-class-test.ts
+++ tests/unit/initializers/body-class-test.ts
@@ -6,8 +6,15 @@ import { module, test } from 'qunit';
 import Resolver from 'ember-resolver';
 import { run } from '@ember/runloop';
 
+import type { TestContext } from '@ember/test-helpers';
+
+interface Context extends TestContext {
+  TestApplication: typeof Application;
+  application: Application;
+}
+
 module('Unit | Initializer | body-class', function (hooks) {
-  hooks.beforeEach(function () {
+  hooks.beforeEach(function (this: Context) {
     this.TestApplication = class TestApplication extends Application {
       modulePrefix = config.modulePrefix;
       podModulePrefix = config.podModulePrefix;
@@ -24,12 +31,12 @@ module('Unit | Initializer | body-class', function (hooks) {
     });
   });
 
-  hooks.afterEach(function () {
+  hooks.afterEach(function (this: Context) {
     run(this.application, 'destroy');
   });
 
   // TODO: Replace this with your real tests.
-  test('it works', async function (assert) {
+  test('it works', async function (this: Context, assert) {
     await this.application.boot();
 
     assert.ok(true);
