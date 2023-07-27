86c19e5cddf082fee160097983fae7d5300648c6
Charles Fries <charliefries@icloud.com>
2023-07-27 10:04:15 -0700

    WIP

---
+++ types/@fortawesome/ember-fontawesome/components/fa-icon.d.ts
@@ -0,0 +1,12 @@
+import Component from '@glimmer/component';
+
+export default class FaIcon extends Component<{
+  Element: HTMLElement;
+  Args: {
+    icon: string;
+    fixedWidth?: boolean;
+    size?: string;
+    prefix?: string;
+    transform?: string;
+  };
+}> {}
