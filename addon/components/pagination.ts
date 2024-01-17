1982126666ff776d2be623df67c8feb0c2a7d0ec
Charles Fries <charliefries@icloud.com>
2024-01-17 15:48:58 -0800

    Pagination

--- addon/components/pagination.ts
+++ addon/components/pagination.ts
@@ -10,7 +10,20 @@ export interface PaginationSignature {
   Element: HTMLElement;
 }
 
-export default class Pagination extends Component<PaginationSignature> {}
+export default class Pagination extends Component<PaginationSignature> {
+  get pages(): number[] {
+    const num = Math.ceil(this.args.totalItems / this.args.perPage);
+    return new Array(num);
+  }
+
+  get canPrev(): boolean {
+    return this.args.curPage > 0;
+  }
+
+  get canNext(): boolean {
+    return this.args.curPage < this.pages.length - 1;
+  }
+}
 
 declare module '@glint/environment-ember-loose/registry' {
   export default interface Registry {
