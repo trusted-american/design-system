507a67a0b9dc22d90c8c511101dabaf327eaa8d6
Charles Fries <charliefries@icloud.com>
2024-01-17 16:29:31 -0800

    Progress

--- addon/components/progress/bar.ts
+++ addon/components/progress/bar.ts
@@ -23,5 +23,6 @@ export default class ProgressBar extends Component<ProgressBarSignature> {
 declare module '@glint/environment-ember-loose/registry' {
   export default interface Registry {
     'Progress::Bar': typeof ProgressBar;
+    'progress/bar': typeof ProgressBar;
   }
 }
