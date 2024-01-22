11cbe97a1250a58bd0672a57ebe8451dd6f4164a
Charles Fries <charliefries@icloud.com>
2024-01-22 09:49:24 -0800

    Remove nav yield linkto (#50)
    * Remove nav yield linkto
    
    * Bump packages
    
    * Ch

--- tests/dummy/app/controllers/comps/modal.ts
+++ tests/dummy/app/controllers/comps/modal.ts
@@ -1,5 +1,11 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
 
 export default class CompsModalController extends Controller {
   showModal = false;
+
+  @action
+  close(): void {
+    this.showModal = false;
+  }
 }
