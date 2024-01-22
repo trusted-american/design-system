e6f64f89b3650797aa6cd238a52e03730e240fae
Charles Fries <charliefries@icloud.com>
2024-01-21 17:44:52 -0800

    Ch

--- tests/dummy/app/controllers/comps/flyout.ts
+++ tests/dummy/app/controllers/comps/flyout.ts
@@ -1,5 +1,11 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
 
 export default class CompsFlyoutController extends Controller {
   showFlyout = false;
+
+  @action
+  close(): void {
+    this.showFlyout = false;
+  }
 }
