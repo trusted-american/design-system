7f72bd74cbbe43cc50bbb8d191104c06573898a0
Charles Fries <charliefries@icloud.com>
2024-01-23 12:52:04 -0800

    Fix tests

--- tests/integration/components/dropdown/header-test.ts
+++ tests/integration/components/dropdown/header-test.ts
@@ -7,20 +7,8 @@ module('Integration | Component | dropdown/header', function (hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function (assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+    await render(hbs`<Dropdown::Header @text="Text" />`);
 
-    await render(hbs`<Dropdown::Header />`);
-
-    assert.dom().hasText('');
-
-    // Template block usage:
-    await render(hbs`
-      <Dropdown::Header>
-        template block text
-      </Dropdown::Header>
-    `);
-
-    assert.dom().hasText('template block text');
+    assert.dom().hasText('Text');
   });
 });
--- tests/integration/components/icon-test.ts
+++ tests/integration/components/icon-test.ts
@@ -7,20 +7,8 @@ module('Integration | Component | icon', function (hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function (assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+    await render(hbs`<Icon @icon="book" />`);
 
-    await render(hbs`<Icon />`);
-
-    assert.dom().hasText('');
-
-    // Template block usage:
-    await render(hbs`
-      <Icon>
-        template block text
-      </Icon>
-    `);
-
-    assert.dom().hasText('template block text');
+    assert.dom('svg').exists();
   });
 });
