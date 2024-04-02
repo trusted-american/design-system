0157b4fdb818dca15f430dfdc501c2faebe98b37
Charles Fries <charliefries@icloud.com>
2024-04-02 09:44:34 -0700

    Ch

--- tests/dummy/app/routes/comps/table.ts
+++ tests/dummy/app/routes/comps/table.ts
@@ -1,7 +1,13 @@
 import Route from '@ember/routing/route';
 
+export interface User {
+  email: string;
+  firstName: string;
+  lastName: string;
+}
+
 export default class CompsTableRoute extends Route {
-  model() {
+  model(): User[] {
     const alphabet = 'abcdefghijklmnopqrstuvwxyz';
     return [...alphabet].map((char) => ({
       email: `${char}@example.com`,
