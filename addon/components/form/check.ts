50453b279c94ab003abc0d9bc185a53b8705ca48
Charles Fries <charliefries@icloud.com>
2024-04-09 13:09:08 -0700

    Check inputOnly

--- addon/components/form/check.ts
+++ addon/components/form/check.ts
@@ -6,6 +6,7 @@ export interface FormCheckSignature {
     label: string;
     identifier: string;
     required?: boolean;
+    inputOnly?: boolean;
     inline?: boolean;
     switch?: boolean;
     help?: string;
