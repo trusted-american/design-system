2a7f3e0c34fa4192cb52ed7bd3b2b9410cd21f29
Charles Fries <charliefries@icloud.com>
2024-04-10 12:41:50 -0700

    Ch

--- tests/integration/components/card/header-test.ts
+++ tests/integration/components/card/header-test.ts
@@ -7,14 +7,6 @@ module('Integration | Component | card/header', function (hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function (assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
-    await render(hbs`<Card::Header />`);
-
-    assert.dom().hasText('');
-
-    // Template block usage:
     await render(hbs`
       <Card::Header>
         template block text
