c4f12ff813d6fb7ab1d16ad46ae0dba578a0b9ba
Charles Fries <charliefries@icloud.com>
2024-04-17 16:30:41 -0700

    Fix test

--- tests/integration/components/nav/item-test.ts
+++ tests/integration/components/nav/item-test.ts
@@ -7,16 +7,8 @@ module('Integration | Component | nav/item', function (hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function (assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
-    await render(hbs`<Nav::Item />`);
-
-    assert.dom().hasText('');
-
-    // Template block usage:
     await render(hbs`
-      <Nav::Item>
+      <Nav::Item @route='index'>
         template block text
       </Nav::Item>
     `);
