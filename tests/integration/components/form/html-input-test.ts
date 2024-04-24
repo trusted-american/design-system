9d596c503f506596b76f917e008b3fa6957f4dff
Charles Fries <charliefries@icloud.com>
2024-04-24 11:27:09 -0700

    Ch

--- tests/integration/components/form/html-input-test.ts
+++ tests/integration/components/form/html-input-test.ts
@@ -1,29 +1,21 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'dummy/tests/helpers';
-import { render, click, type TestContext } from '@ember/test-helpers';
+import { render, click } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
-import type { FormInputSignature } from '@trusted-american/design-system/components/form/input';
-type Context = FormInputSignature['Args'] & TestContext;
 
 module('Integration | Component | form/html-input', function (hooks) {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
   setupRenderingTest(hooks);
 
-  test('it renders', async function (this: Context, assert) {
-    this.value = 'value';
-
-    await render<Context>(hbs`
-    <Form::HtmlInput
-      @value={{this.value}}
-      @label="Label"
-      @identifier="textarea"
-      @required={{true}}
-      @inputOnly={{false}}
-      @help="Help"
-      @onChange={{fn (mut this.value)}}
-      placeholder="Placeholder"
-    />
-  `);
+  test('it renders', async function (assert) {
+    await render(hbs`
+      <Form::HtmlInput
+        @value='value'
+        @label='Label'
+        @identifier='identifier'
+        @required={{true}}
+        @onChange={{(noop)}}
+      />
+    `);
 
     assert.dom().includesText('Label *');
     assert.dom().includesText('Please provide a value.');
