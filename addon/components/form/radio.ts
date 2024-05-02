4e5d39aade4b17a60c2597965567330c0e9e036c
Charles Fries <charliefries@icloud.com>
2024-05-02 16:24:37 -0700

    RadioOption

--- addon/components/form/radio.ts
+++ addon/components/form/radio.ts
@@ -3,8 +3,12 @@ import Component from '@glimmer/component';
 import type { BaseArgs } from './input';
 import type { Option } from '@trusted-american/design-system/components/form/select';
 
+interface RadioOption<T> extends Option<T> {
+  help?: string;
+}
+
 interface Args<T> extends BaseArgs {
-  options: Option<T>[];
+  options: RadioOption<T>[];
   selected: T | null | undefined;
   inline?: boolean;
   onChange: (selected: T) => void;
