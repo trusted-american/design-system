b29522214925abc67f4ab92344b75ce8fcebcc04
Charles Fries <charliefries@icloud.com>
2024-06-12 12:39:05 -0700

    Cleanup

--- tests/dummy/app/routes/comps/calendar.ts
+++ tests/dummy/app/routes/comps/calendar.ts
@@ -1,7 +1,9 @@
 import Route from '@ember/routing/route';
 
+import type { Event } from '@trusted-american/design-system/components/calendar';
+
 export default class CompsCalendarRoute extends Route {
-  model() {
+  model(): Event[] {
     return [
       { title: 'Event 1', start: new Date() },
       { title: 'Event 2', start: new Date(), backgroundColor: '#5af542' },
