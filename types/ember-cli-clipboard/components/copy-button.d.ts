24a1d2323c349db6dfcb6f81305b6bbfaa12432a
Charles Fries <charliefries@icloud.com>
2024-09-19 14:23:09 -0700

    ember-cli-clipboard args

--- types/ember-cli-clipboard/components/copy-button.d.ts
+++ types/ember-cli-clipboard/components/copy-button.d.ts
@@ -3,7 +3,13 @@ import Component from '@glimmer/component';
 export default class CopyButton extends Component<{
   Args: {
     text: unknown;
+    target?: string;
+    action?: 'copy' | 'cut';
+    container?: string | Element;
     delegateClickEvent?: boolean;
+    buttonType?: string;
+    onSuccess?: () => void;
+    onError?: () => void;
   };
   Blocks: {
     default: [];
