090fa6bb44ecd03c457f02fd0f9745473b371a77
Daniel Pasion <danielpasion@Daniels-MacBook-Pro.local>
2024-12-23 14:51:01 -0800

    button-group-test

--- tests/integration/components/button-group-test.ts
+++ tests/integration/components/button-group-test.ts
@@ -14,5 +14,32 @@ module('Integration | Component | button-group', function (hooks) {
     `);
 
     assert.dom().hasText('template block text');
+
+    await render(hbs`
+      <ButtonGroup>
+        <Button
+          @text="Hi"
+        />
+        <Button
+          @text="Actually"
+        />
+        <Button
+          @text="Bye"
+        />
+
+        <Button
+          @text="This Shouldnt Show Up"
+          @icon="house"
+          @isIconOnly={{true}}
+        />
+    </ButtonGroup>`);
+
+    assert.dom('button:nth-of-type(1)').hasText('Hi');
+    assert.dom('button:nth-of-type(2)').hasText('Actually');
+    assert.dom('button:nth-of-type(3)').hasText('Bye');
+    assert
+      .dom('button:nth-of-type(4)')
+      .doesNotContainText('This Should Show Up');
+    assert.dom('button:nth-of-type(4) svg').exists();
   });
 });
