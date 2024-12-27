874338fc09052bef7818150450ef8872b3c85ec1
Daniel Pasion <danielpasion@Daniels-MacBook-Pro.local>
2024-12-27 11:37:37 -0800

    pagination-test

--- tests/integration/components/pagination-test.ts
+++ tests/integration/components/pagination-test.ts
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'dummy/tests/helpers';
-import { render, type TestContext } from '@ember/test-helpers';
+import { click, render, type TestContext } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
 import type { PaginationSignature } from '@trusted-american/design-system/components/pagination';
@@ -10,10 +10,11 @@ type Context = PaginationSignature['Args'] & TestContext;
 module('Integration | Component | pagination', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function (assert) {
+  test('it renders', async function (this: Context, assert) {
+    this.curPage = 0;
     await render<Context>(hbs`
       <Pagination
-        @curPage={{0}}
+        @curPage={{this.curPage}}
         @pageSize={{3}}
         @totalItems={{9}}
         @onChange={{fn (mut this.curPage)}}
@@ -21,5 +22,21 @@ module('Integration | Component | pagination', function (hooks) {
     `);
 
     assert.dom().hasText('Previous 1 2 3 Next');
+    assert.dom('ul li:nth-of-type(1)').hasText('Previous');
+    assert.dom('ul li:nth-of-type(2)').hasText('1');
+    assert.dom('ul li:nth-of-type(3)').hasText('2');
+    assert.dom('ul li:nth-of-type(4)').hasText('3');
+    assert.dom('ul li:nth-of-type(5)').hasText('Next');
+
+    assert.deepEqual(this.curPage, 0);
+
+    await click('ul li:nth-of-type(3) a');
+    assert.deepEqual(this.curPage, 1);
+
+    await click('ul li:nth-of-type(5) a');
+    assert.deepEqual(this.curPage, 2);
+
+    await click('ul li:nth-of-type(1) a');
+    assert.deepEqual(this.curPage, 1);
   });
 });
