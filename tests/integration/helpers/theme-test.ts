368dfc41079b7cdf5b030f3183a1841a55da35a3
Daniel Pasion <danielpasion@Daniels-MacBook-Pro.local>
2025-01-06 14:55:42 -0800

    update theme-test

--- tests/integration/helpers/theme-test.ts
+++ tests/integration/helpers/theme-test.ts
@@ -2,6 +2,7 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'dummy/tests/helpers';
 import { render } from '@ember/test-helpers';
 import Theme from '@trusted-american/design-system/helpers/theme';
+import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Helper | theme', function (hooks) {
   setupRenderingTest(hooks);
@@ -12,43 +13,29 @@ module('Integration | Helper | theme', function (hooks) {
 
   test('it renders dark', async function (assert) {
     localStorage.setItem('isDarkMode', 'dark');
-    await render(
-      <template>
-        <div>
-          {{theme}}
-          <h1>Hello World</h1>
-          <h2>Goodbye World</h2>
-        </div>
-      </template>,
-    );
+
+    await render(hbs`{{theme}}`);
+
     const element = document.querySelectorAll('.highcharts-dark');
     assert.dom(element[0]).exists();
-    assert.equal(element[1], undefined);
+    assert.deepEqual(element[1], undefined);
 
     const elementShouldnotExist =
       document.querySelectorAll('.highcharts-light');
-    assert.equal(elementShouldnotExist[1], undefined);
+    assert.deepEqual(elementShouldnotExist[1], undefined);
 
     localStorage.removeItem('isDarkMode');
   });
 
   test('it renders back to light', async function (assert) {
     localStorage.setItem('isDarkMode', 'light');
-    await render(
-      <template>
-        <div>
-          {{theme}}
-          <h1>Hello World</h1>
-          <h2>Goodbye World</h2>
-        </div>
-      </template>,
-    );
+    await render(hbs`{{theme}}`);
     const element = document.querySelectorAll('.highcharts-light');
     assert.dom(element[0]).exists();
-    assert.equal(element[1], undefined);
+    assert.deepEqual(element[1], undefined);
 
     const elementShouldnotExist = document.querySelectorAll('.highcharts-dark');
-    assert.equal(elementShouldnotExist[1], undefined);
+    assert.deepEqual(elementShouldnotExist[1], undefined);
 
     localStorage.removeItem('isDarkMode');
   });
