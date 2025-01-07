1487425518d39a3e2b2d48c852898769b692359f
Charles Fries <charliefries@icloud.com>
2025-01-06 17:25:51 -0800

    Cleanup

--- tests/dummy/app/controllers/application.ts
+++ tests/dummy/app/controllers/application.ts
@@ -1,20 +1,12 @@
 import Controller from '@ember/controller';
-import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 
 export default class ApplicationController extends Controller {
   @tracked theme: 'light' | 'dark' = 'light';
 
-  @action
-  toggleDarkTheme(event: Event): void {
-    event.stopPropagation();
-    this.isDarkTheme = !this.isDarkTheme;
-  }
-
   get isDarkTheme(): boolean {
     return this.theme === 'dark';
   }
-
   set isDarkTheme(value: boolean) {
     this.theme = value ? 'dark' : 'light';
   }
