a62c73444c3d19aa2fbe847368726c71116c583a
Charles Fries <charliefries@icloud.com>
2025-01-10 17:14:36 -0800

    feat: add snippets, add body-class initializer test (#100)
    * Ch
    
    * Ch
    
    * Ch

--- tests/dummy/app/controllers/helpers.ts
+++ tests/dummy/app/controllers/helpers.ts
@@ -1,5 +1,17 @@
 import Controller from '@ember/controller';
+import { tracked } from '@glimmer/tracking';
+
+import type { Theme } from '@trusted-american/design-system/helpers/theme';
 
 export default class HelpersController extends Controller {
   date = new Date();
+
+  @tracked theme: Theme = 'light';
+
+  get isDarkTheme(): boolean {
+    return this.theme === 'dark';
+  }
+  set isDarkTheme(value: boolean) {
+    this.theme = value ? 'dark' : 'light';
+  }
 }
