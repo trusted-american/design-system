e22f0c122e8edbc0843a24f54a08541bdefc5e8c
DanielPasion <dnpaxion@gmail.com>
2025-01-10 16:33:20 -0800

    progress, property-list, ratio

--- tests/integration/components/property-list-test.ts
+++ tests/integration/components/property-list-test.ts
@@ -7,20 +7,80 @@ module('Integration | Component | property-list', function (hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function (assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+    await render(hbs`<PropertyList as |list|>
+      <list.item
+        @label="Key 1"
+        @value="Value 1"
+        @alt={{false}}
+        @tooltip="This is a tooltip."
+      >
+        Value 1
+      </list.item>
+      <list.item @label="Key 2" @value="Value 2" @alt={{false}}>
+        Value 2
+      </list.item>
+      <list.item @label="Key 3" @value="Value 3" @alt={{false}}>
+        <:default>
+          Value 3
+        </:default>
+        <:side>
+          Side
+        </:side>
+      </list.item>
+    </PropertyList>`);
 
-    await render(hbs`<PropertyList />`);
+    // await this.pauseTest();
 
-    assert.dom('div').hasText('');
+    assert.dom('dl dt:nth-of-type(1)').hasText('Key 1');
+    assert.dom('dl dd:nth-of-type(1)').hasText('Value 1');
 
-    // Template block usage:
-    await render(hbs`
-      <PropertyList>
-        template block text
-      </PropertyList>
-    `);
+    assert.dom('dl dt:nth-of-type(2)').hasText('Key 2');
+    assert.dom('dl dd:nth-of-type(2)').hasText('Value 2');
 
-    assert.dom('div').hasText('template block text');
+    assert.dom('dl dt:nth-of-type(3)').hasText('Key 3');
+    assert.dom('dl dd:nth-of-type(3)').hasText('Value 3 Side');
+  });
+
+  test('it renders horizontal', async function (assert) {
+    await render(hbs`<PropertyList @isHorizontal={{true}}  as |list|>
+      <list.item
+        @label="Key 1"
+        @value="Value 1"
+        @alt={{false}}
+        @tooltip="This is a tooltip."
+      >
+        Value 1
+      </list.item>
+      <list.item @label="Key 2" @value="Value 2" @alt={{false}}>
+        Value 2
+      </list.item>
+      <list.item @label="Key 3" @value="Value 3" @alt={{false}}>
+        <:default>
+          Value 3
+        </:default>
+        <:side>
+          Side
+        </:side>
+      </list.item>
+    </PropertyList>`);
+
+    assert.dom('dl div:nth-of-type(1) dt').hasText('Key 1');
+    assert.dom('dl div:nth-of-type(1) dd').hasText('Value 1');
+
+    assert.dom('dl div:nth-of-type(2) dt').hasText('Key 2');
+    assert.dom('dl div:nth-of-type(2) dd').hasText('Value 2');
+
+    assert.dom('dl div:nth-of-type(3) dt').hasText('Key 3');
+    assert.dom('dl div:nth-of-type(3) dd').hasText('Value 3 Side');
+
+    // await this.pauseTest();
+  });
+
+  test('it renders with title', async function (assert) {
+    await render(hbs`<PropertyList @title='Test'>
+
+    </PropertyList>`);
+
+    assert.dom().hasText('Test');
   });
 });
--- tests/integration/components/ratio-test.ts
+++ tests/integration/components/ratio-test.ts
@@ -1,18 +1,88 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'dummy/tests/helpers';
-import { render } from '@ember/test-helpers';
+import { find, render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Component | ratio', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function (assert) {
+  test('it renders 1 x 1', async function (assert) {
     await render(hbs`
       <Ratio @ratio='1x1'>
-        template block text
       </Ratio>
     `);
 
-    assert.dom().hasText('template block text');
+    const element = find('[data-test-ratio]');
+    assert.ok(element, '.bg-danger exists');
+    const width = element
+      ? window.getComputedStyle(element).width
+      : 'errorWidth';
+    const height = element
+      ? window.getComputedStyle(element).height
+      : 'errorHeight';
+
+    const numericWidth = Number(width.substring(0, width.length - 2));
+    const numericHeight = Number(height.substring(0, height.length - 2));
+    assert.deepEqual(numericWidth, numericHeight);
+  });
+
+  test('it renders 4 x 3', async function (assert) {
+    await render(hbs`
+      <Ratio @ratio='4x3'>
+      </Ratio>
+    `);
+
+    const element = find('[data-test-ratio]');
+    assert.ok(element, '.bg-danger exists');
+    const width = element
+      ? window.getComputedStyle(element).width
+      : 'errorWidth';
+    const height = element
+      ? window.getComputedStyle(element).height
+      : 'errorHeight';
+
+    const numericWidth = Number(width.substring(0, width.length - 2));
+    const numericHeight = Number(height.substring(0, height.length - 2));
+    assert.deepEqual(numericWidth / numericHeight, 4 / 3);
+  });
+
+  test('it renders 16 x 9', async function (assert) {
+    await render(hbs`
+      <Ratio @ratio='16x9'>
+      </Ratio>
+    `);
+
+    const element = find('[data-test-ratio]');
+    assert.ok(element, '.bg-danger exists');
+    const width = element
+      ? window.getComputedStyle(element).width
+      : 'errorWidth';
+    const height = element
+      ? window.getComputedStyle(element).height
+      : 'errorHeight';
+
+    const numericWidth = Number(width.substring(0, width.length - 2));
+    const numericHeight = Number(height.substring(0, height.length - 2));
+    assert.deepEqual(numericWidth / numericHeight, 16 / 9);
+  });
+
+  test('it renders 21 x 9', async function (assert) {
+    await render(hbs`
+      <Ratio @ratio='21x9'>
+      </Ratio>
+    `);
+
+    const element = find('[data-test-ratio]');
+    assert.ok(element, '.bg-danger exists');
+    const width = element
+      ? window.getComputedStyle(element).width
+      : 'errorWidth';
+    const height = element
+      ? window.getComputedStyle(element).height
+      : 'errorHeight';
+
+    const numericWidth = Number(width.substring(0, width.length - 2));
+    const numericHeight = Number(height.substring(0, height.length - 2));
+    assert.deepEqual(numericWidth / numericHeight, 21 / 9);
   });
 });
