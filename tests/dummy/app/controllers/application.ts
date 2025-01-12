cd98c7148ece54b32a2739d961af9c16dad1dba2
Charles Fries <charliefries@icloud.com>
2025-01-11 16:59:07 -0800

    Revert

--- tests/dummy/app/controllers/application.ts
+++ tests/dummy/app/controllers/application.ts
@@ -3,9 +3,7 @@ import { tracked } from '@glimmer/tracking';
 
 import type { Theme } from '@trusted-american/design-system/helpers/theme';
 
-export default class HelpersController extends Controller {
-  date = new Date();
-
+export default class ApplicationController extends Controller {
   @tracked theme: Theme = 'light';
 
   get isDarkTheme(): boolean {
