10e6a672b901b5663ce61ceeaca3503fba17cc8b
Charles Fries <charliefries@icloud.com>
2025-01-22 15:20:32 -0800

    Improve form input tests

--- tests/integration/components/form/input-test.ts
+++ tests/integration/components/form/input-test.ts
@@ -11,7 +11,7 @@ module('Integration | Component | form/input', function (hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function (this: Context, assert) {
-    this.value = 'Value';
+    this.value = '';
 
     await render<Context>(hbs`
       <Form::Input
@@ -31,6 +31,49 @@ module('Integration | Component | form/input', function (hooks) {
     assert.dom('[data-test-form-input]').isRequired();
     assert.dom('[data-test-form-help]').exists();
     assert.dom('[data-test-form-error]').exists();
+  });
+
+  test('it default to text type', async function (this: Context, assert) {
+    this.value = '';
+
+    await render<Context>(hbs`
+      <Form::Input
+        @value={{this.value}}
+        @label=""
+        @identifier=""
+        @onChange={{fn (mut this.value)}}
+      />
+    `);
+
+    assert.dom('[data-test-form-input]').hasAttribute('type', 'text');
+  });
+
+  test('it loads initial value', async function (this: Context, assert) {
+    this.value = 'Value';
+
+    await render<Context>(hbs`
+      <Form::Input
+        @value={{this.value}}
+        @label=""
+        @identifier=""
+        @onChange={{fn (mut this.value)}}
+      />
+    `);
+
+    assert.dom('[data-test-form-input]').hasValue('Value');
+  });
+
+  test('it updates value', async function (this: Context, assert) {
+    this.value = '';
+
+    await render<Context>(hbs`
+      <Form::Input
+        @value={{this.value}}
+        @label=""
+        @identifier=""
+        @onChange={{fn (mut this.value)}}
+      />
+    `);
 
     await fillIn('[data-test-form-input]', 'test');
 
