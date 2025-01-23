043832e372f1835d25f3df7fdb866d9a026eb93a
Charles Fries <charliefries@icloud.com>
2025-01-22 21:20:38 -0800

    Ch

--- tests/integration/modifiers/did-insert-test.ts
+++ tests/integration/modifiers/did-insert-test.ts
@@ -1,14 +1,20 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, type TestContext } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
+
+interface Context extends TestContext {
+  fn: () => void;
+}
 
 module('Integration | Modifier | did-insert', function (hooks) {
   setupRenderingTest(hooks);
 
   // Replace this with your real tests.
-  test('it renders', async function (assert) {
-    await render(hbs`<div {{did-insert}}></div>`);
+  test('it renders', async function (this: Context, assert) {
+    this.fn = () => {};
+
+    await render<Context>(hbs`<div {{did-insert this.fn}}></div>`);
 
     assert.ok(true);
   });
