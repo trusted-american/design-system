02b180f9449bd31e194f6e3e660d90403795fc01
Charles Fries <charliefries@icloud.com>
2025-01-27 21:25:36 -0800

    Cusor pagination

--- tests/dummy/app/controllers/comps/pagination.ts
+++ tests/dummy/app/controllers/comps/pagination.ts
@@ -1,5 +1,12 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
 
 export default class CompsPaginationController extends Controller {
   page = 0;
+
+  @action
+  next() {}
+
+  @action
+  previous() {}
 }
