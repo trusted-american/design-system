788ee69990cedb94e42a75c4226348b730517320
Charles Fries <charliefries@icloud.com>
2025-02-02 12:52:25 -0800

    WIP

--- addon/components/list-filter.ts
+++ addon/components/list-filter.ts
@@ -1,5 +1,6 @@
 import Component from '@glimmer/component';
-import { action, set } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 import dayjs from 'dayjs';
 
 import type { Option } from '@trusted-american/design-system/components/form/select';
@@ -73,6 +74,108 @@ type InternalPredicate<T> =
   | _RangeDatePredicate
   | _SingleDatePredicate;
 
+class _InternalPredicate<T> {
+  _predicate: InternalPredicate<T>;
+
+  @tracked isEnabled = false;
+
+  // @tracked mode?: InternalPredicate<T>['mode'];
+  // value: PredicateParams['value'];
+
+  @tracked _value: InternalPredicate<T>['value'];
+
+  constructor(predicate: Predicate<T>) {
+    this._predicate = { ...predicate } as InternalPredicate<T>;
+
+    this._value = predicate.value;
+
+    const { value: val } = predicate;
+
+    if ((val || val === false) && (!Array.isArray(val) || val.length !== 0)) {
+      this.isEnabled = true;
+    }
+
+    if (this._predicate.type !== 'date') {
+      return;
+    }
+
+    const { value } = this._predicate;
+
+    if (Array.isArray(value)) {
+      this._predicate.mode = 'inTheLast';
+      this._predicate.valueA = 1;
+      (this._predicate as _OffsetDatePredicate).valueB = 'days';
+      return;
+    } else if (value.gte && value.lte) {
+      this._predicate.mode = 'between';
+      this._predicate.valueA = value.gte;
+      (this._predicate as _RangeDatePredicate).valueB = value.lte;
+    } else if (value.gt) {
+      this._predicate.mode = 'isAfter';
+      this._predicate.valueA = value.gt;
+    } else if (value.gte) {
+      this._predicate.mode = 'isAfterOrOn';
+      this._predicate.valueA = value.gte;
+    } else if (value.lt) {
+      this._predicate.mode = 'isBefore';
+      this._predicate.valueA = value.lt;
+    } else if (value.lte) {
+      this._predicate.mode = 'isBeforeOrOn';
+      this._predicate.valueA = value.lte;
+    }
+  }
+
+  get value() {
+    if (!this.isEnabled) {
+      if (this._predicate.type === 'date' || this._predicate.type === 'multi') {
+        return [];
+      }
+      return undefined;
+    }
+
+    if (this._predicate.type !== 'date') {
+      return this._value;
+    }
+
+    if (Array.isArray(this._value)) {
+      return [];
+    }
+
+    const value: DateRangeQueryParam = {
+      gte: null,
+      gt: null,
+      lt: null,
+      lte: null,
+    };
+
+    if (this._predicate.mode === 'inTheLast') {
+      value.gte = dayjs()
+        .subtract(
+          this._predicate.valueA as number,
+          this._predicate.valueB === 'months' ? 'months' : 'days',
+        )
+        .toDate();
+      value.lte = new Date();
+    } else if (this._predicate.mode === 'equals') {
+      value.gte = this._predicate.valueA;
+      value.lte = dayjs(this._predicate.valueA).endOf('day').toDate();
+    } else if (this._predicate.mode === 'between') {
+      value.gte = this._predicate.valueA;
+      value.lte = this._predicate.valueB;
+    } else if (this._predicate.mode === 'isAfter') {
+      value.gt = this._predicate.valueA;
+    } else if (this._predicate.mode === 'isAfterOrOn') {
+      value.gte = this._predicate.valueA;
+    } else if (this._predicate.mode === 'isBefore') {
+      value.lt = this._predicate.valueA;
+    } else {
+      value.lte = this._predicate.valueA;
+    }
+
+    return value;
+  }
+}
+
 export interface ListFilterSignature<T> {
   Args: {
     predicates: Predicate<T>[];
@@ -100,66 +203,14 @@ export interface ListFilterSignature<T> {
 }
 
 export default class ListFilter<T> extends Component<ListFilterSignature<T>> {
-  predicates: InternalPredicate<T>[];
+  predicates: _InternalPredicate<T>[];
 
   constructor(owner: unknown, args: ListFilterSignature<T>['Args']) {
     super(owner, args);
 
-    this.predicates = this.args.predicates.map((predicate) => {
-      if (predicate.type !== 'date') {
-        return { ...predicate };
-      }
-
-      if (Array.isArray(predicate.value)) {
-        return {
-          ...predicate,
-          mode: 'inTheLast',
-          valueA: 1,
-          valueB: 'days',
-        };
-      }
-
-      const { value } = predicate;
-      if (value.gte && value.lte) {
-        return {
-          ...predicate,
-          mode: 'between',
-          valueA: value.gte,
-          valueB: value.lte,
-        };
-      } else if (value.gt) {
-        return {
-          ...predicate,
-          mode: 'isAfter',
-          valueA: value.gt,
-        };
-      } else if (value.gte) {
-        return {
-          ...predicate,
-          mode: 'isAfterOrOn',
-          valueA: value.gte,
-        };
-      } else if (value.lt) {
-        return {
-          ...predicate,
-          mode: 'isBefore',
-          valueA: value.lt,
-        };
-      } else if (value.lte) {
-        return {
-          ...predicate,
-          mode: 'isBeforeOrOn',
-          valueA: value.lte,
-        };
-      } else {
-        return {
-          ...predicate,
-          mode: 'inTheLast',
-          valueA: 1,
-          valueB: 'days',
-        };
-      }
-    });
+    this.predicates = this.args.predicates.map(
+      (predicate) => new _InternalPredicate(predicate),
+    );
   }
 
   get selections(): Predicate<T>[] {
@@ -168,41 +219,6 @@ export default class ListFilter<T> extends Component<ListFilterSignature<T>> {
         (Boolean(value) || value === false) &&
         (!Array.isArray(value) || value.length),
     );
-  }
-
-  @action
-  toggle(predicate: InternalPredicate<T>, checked: boolean): void {
-    if (checked) {
-      if (predicate.type === 'multi') {
-        const [option] = predicate.options;
-        const value = option ? [option.value] : [];
-        set(predicate, 'value', value);
-      } else if (predicate.type === 'string') {
-        set(predicate, 'value', 'Text');
-      } else if (predicate.type === 'date') {
-        const djs = dayjs();
-        const valueA = djs.startOf('day').toDate();
-        const valueB = djs.endOf('day').toDate();
-        set(predicate, 'value', {
-          gte: valueA,
-          gt: null,
-          lt: null,
-          lte: valueB,
-        });
-        set(predicate, 'mode', 'between');
-        set(predicate, 'valueA', valueA);
-        set(predicate as _RangeDatePredicate, 'valueB', valueB);
-      } else {
-        const [option] = predicate.options;
-        set(predicate, 'value', option?.value);
-      }
-    } else {
-      if (predicate.type === 'date' || predicate.type === 'multi') {
-        set(predicate, 'value', []);
-      } else {
-        set(predicate, 'value', undefined);
-      }
-    }
   }
 
   @action
@@ -226,10 +242,8 @@ export default class ListFilter<T> extends Component<ListFilterSignature<T>> {
   @action
   clear(): void {
     for (const predicate of this.predicates) {
-      this.toggle(predicate, false);
-
-      const { key, value } = predicate;
-      this.args.onChange(key, value);
+      predicate.isEnabled = false;
+      this.args.onChange(predicate._predicate.key, predicate.value);
     }
   }
 
@@ -244,51 +258,14 @@ export default class ListFilter<T> extends Component<ListFilterSignature<T>> {
     }
     form.classList.remove('was-validated');
 
-    const predicates = this.predicates.map((predicate) => {
-      if (predicate.type === 'date' && !Array.isArray(predicate.value)) {
-        const value: DateRangeQueryParam = {
-          gte: null,
-          gt: null,
-          lt: null,
-          lte: null,
-        };
-
-        if (predicate.mode === 'inTheLast') {
-          value.gte = dayjs()
-            .subtract(
-              predicate.valueA as number,
-              predicate.valueB === 'months' ? 'months' : 'days',
-            )
-            .toDate();
-          value.lte = new Date();
-        } else if (predicate.mode === 'equals') {
-          value.gte = predicate.valueA;
-          value.lte = dayjs(predicate.valueA).endOf('day').toDate();
-        } else if (predicate.mode === 'between') {
-          value.gte = predicate.valueA;
-          value.lte = predicate.valueB;
-        } else if (predicate.mode === 'isAfter') {
-          value.gt = predicate.valueA;
-        } else if (predicate.mode === 'isAfterOrOn') {
-          value.gte = predicate.valueA;
-        } else if (predicate.mode === 'isBefore') {
-          value.lt = predicate.valueA;
-        } else {
-          value.lte = predicate.valueA;
-        }
-        set(predicate, 'value', value);
-      }
-      return predicate;
-    });
-
-    for (const { key, value } of predicates) {
-      this.args.onChange(key, value);
+    for (const predicate of this.predicates) {
+      this.args.onChange(predicate._predicate.key, predicate.value);
     }
   }
 
   @action
-  change(predicate: Predicate, opt: Option<T>) {
-    set(predicate, 'value', opt.value);
+  change(predicate: _InternalPredicate<T>, opt: Option<T>) {
+    predicate._value = opt.value;
   }
 }
 
