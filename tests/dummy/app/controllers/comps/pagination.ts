8801f68294949971132d0159e7761f4b3b767687
Charles Fries <charliefries@icloud.com>
2025-02-02 23:08:24 -0800

    Ch

--- tests/dummy/app/controllers/comps/pagination.ts
+++ tests/dummy/app/controllers/comps/pagination.ts
@@ -2,7 +2,7 @@ import Controller from '@ember/controller';
 import { action } from '@ember/object';
 
 export default class CompsPaginationController extends Controller {
-  page1 = 5;
+  page = 5;
   page2 = 0;
 
   @action
