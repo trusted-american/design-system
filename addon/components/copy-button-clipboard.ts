a5a61c0536fd787c9e2a7f847e03fce3c1dfc6d4
Charles Fries <charliefries@icloud.com>
2025-02-05 15:08:03 -0800

    Ch

--- addon/components/copy-button-clipboard.ts
+++ addon/components/copy-button-clipboard.ts
@@ -1,4 +1,4 @@
-// TODO: fixes component name conflict
+// fixes ember-shiki/ember-cli-clipboard copy-button component name conflict
 import CopyButton from 'ember-cli-clipboard/components/copy-button';
 
 export default class CopyButtonClipboard extends CopyButton {}
--- tests/dummy/app/components/copy-button.ts
+++ tests/dummy/app/components/copy-button.ts
@@ -1,2 +1,2 @@
-// TODO: fixes component name conflict
+// fixes ember-shiki/ember-cli-clipboard copy-button component name conflict
 export { default } from 'ember-shiki/components/copy-button';
