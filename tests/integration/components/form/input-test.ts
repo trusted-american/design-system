4f146e99f5060a5519471a93294616da42d91d72
Charles Fries <charliefries@icloud.com>
2025-02-06 14:42:37 -0800

    fix: rearrange form invalid feedback to fix layout (#136)
    * ch
    
    * not working submit
    
    * Validation
    
    * ch
    
    * ch
    
    ---------
    
    Co-authored-by: DanielPasion <dnpaxion@gmail.com>

--- tests/integration/components/form/input-test.ts
+++ tests/integration/components/form/input-test.ts
@@ -1,11 +1,14 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'dummy/tests/helpers';
-import { render, fillIn, type TestContext } from '@ember/test-helpers';
+import { render, fillIn, type TestContext, click } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
 import type { FormInputSignature } from '@trusted-american/design-system/components/form/input';
 
-type Context = FormInputSignature['Args'] & TestContext;
+type Context = FormInputSignature['Args'] &
+  TestContext & {
+    submit: (event: Event) => void;
+  };
 
 module('Integration | Component | form/input', function (hooks) {
   setupRenderingTest(hooks);
@@ -78,5 +81,49 @@ module('Integration | Component | form/input', function (hooks) {
     await fillIn('[data-test-form-input]', 'test');
 
     assert.strictEqual(this.value, 'test');
+  });
+
+  test('invalidFeedback works', async function (this: Context, assert) {
+    this.value = '';
+    this.submit = (event: Event) => {
+      event.preventDefault();
+
+      const { target } = event;
+      if (!(target instanceof HTMLFormElement)) {
+        throw new Error();
+      }
+
+      if (!target.checkValidity()) {
+        target.classList.add('was-validated');
+      } else {
+        target.classList.remove('was-validated');
+        return;
+      }
+    };
+
+    await render<Context>(hbs`
+      <form novalidate {{on "submit" this.submit}}>
+       <Form::Input
+        @value={{this.value}}
+        @label=""
+        @identifier=""
+        @isRequired={{true}}
+        @onChange={{fn (mut this.value)}}
+        @invalidFeedback="Wrong"
+      >
+        <:actions>
+          <button type="button" disabled>Hi</button>
+        </:actions>
+      </Form::Input>
+        <button type="submit" data-test-submit>Submit</button>
+      </form>
+    `);
+
+    assert.dom('[data-test-form-input]').hasStyle({ height: '38px' });
+    assert.dom('form').doesNotHaveClass('was-validated');
+    await click('[data-test-submit]');
+    assert.dom('form').hasClass('was-validated');
+    assert.dom('[data-test-form-error]').hasText('Wrong');
+    assert.dom('[data-test-form-input]').hasStyle({ height: '38px' });
   });
 });
