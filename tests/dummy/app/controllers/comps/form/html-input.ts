227e9c164f01d634784f83a1c75ada3232a0de94
Charles Fries <charliefries@icloud.com>
2025-02-28 13:28:50 -0800

    Ch

--- tests/dummy/app/controllers/comps/form/html-input.ts
+++ tests/dummy/app/controllers/comps/form/html-input.ts
@@ -1,5 +1,5 @@
 import Controller from '@ember/controller';
 
 export default class CompsFormHtmlInputController extends Controller {
-  value: string | null = null;
+  value: string | null = '<p>Hello World!</p>';
 }
