d7143cf160ee33069db880f7b6458648c53ade14
Charles Fries <charliefries@icloud.com>
2025-06-18 13:23:06 -0700

    Uninstall

--- addon/components/copy-button-clipboard.ts
+++
@@ -1,4 +0,0 @@
-// fixes ember-shiki/ember-cli-clipboard copy-button component name conflict
-import CopyButton from 'ember-cli-clipboard/components/copy-button';
-
-export default class CopyButtonClipboard extends CopyButton {}
