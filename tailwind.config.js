324d68d2472b5e4d8b91e08dac3cc4d7c3ff6434
Charles Fries <charliefries@icloud.com>
2025-07-30 18:10:26 -0700

    Install

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './addon/**/*.{gjs,gts,hbs,html,js,ts}',
+    './tests/dummy/app/**/*.{gjs,gts,hbs,html,js,ts}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
