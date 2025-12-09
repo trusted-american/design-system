03df52d8a02380544585054f99755d64f65a81c7
Charles Fries <charliefries@icloud.com>
2025-12-08 16:32:13 -0800

    ember-basic-dropdown

--- tests/dummy/app/app.ts
+++ tests/dummy/app/app.ts
@@ -3,6 +3,7 @@ import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'dummy/config/environment';
 import { importSync, isDevelopingApp, macroCondition } from '@embroider/macros';
+import { setConfig } from 'ember-basic-dropdown/config';
 import './font-awesome';
 
 if (macroCondition(isDevelopingApp())) {
@@ -16,3 +17,7 @@ export default class App extends Application {
 }
 
 loadInitializers(App, config.modulePrefix);
+
+setConfig({
+  rootElement: config.APP['rootElement'] as string,
+});
