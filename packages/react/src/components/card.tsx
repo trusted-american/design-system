77a7a996d5172ed44abc4690fe62d5bbff0d9dc1
Charles Fries <charliefries@icloud.com>
2025-12-17 15:19:29 -0800

    Ch

---
+++ packages/react/src/components/card.tsx
@@ -0,0 +1,28 @@
+import type { ReactNode } from 'react';
+
+interface CardProps {
+  hoverable?: boolean;
+  className?: string;
+  children: ReactNode;
+  onClick?: () => void;
+}
+
+const Card = ({
+  hoverable = true,
+  className,
+  children,
+  onClick,
+}: CardProps) => {
+  return (
+    <div
+      className={`bg-gray-100 rounded-xl overflow-hidden ${
+        hoverable ? 'outline-blue-400 hover:outline-2' : ''
+      } ${className}`}
+      onClick={onClick}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Card;
--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,3 +1,5 @@
 export { default as Alert } from './components/alert';
 export { default as Avatar } from './components/avatar';
 export { default as Button } from './components/button';
+export { default as Card } from './components/card';
+export { default as Spinner } from './components/spinner';
