f8443894e94014f321a7d0e65f9cd37fb8164827
Charles Fries <charliefries@icloud.com>
2025-12-17 17:57:14 -0800

    Ch

--- packages/react/src/components/spinner.tsx
+++ packages/react/src/components/spinner.tsx
@@ -1,3 +1,6 @@
+const spinnerClassName =
+  'w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-400';
+
 interface SpinnerProps {
   loadingLabel: string;
 }
@@ -8,7 +11,7 @@ const Spinner = ({ loadingLabel }: SpinnerProps) => {
       <div role="status">
         <svg
           aria-hidden="true"
-          className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-400"
+          className={spinnerClassName}
           viewBox="0 0 100 101"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
