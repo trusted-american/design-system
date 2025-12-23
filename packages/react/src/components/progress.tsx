6177f406e1f57a6fa280b9ef012490fa153f4aca
Charles Fries <charliefries@icloud.com>
2025-12-23 15:15:29 -0800

    Progress

--- packages/react/src/components/progress.tsx
+++ packages/react/src/components/progress.tsx
@@ -1,7 +1,16 @@
-interface ProgressProps {}
+interface ProgressProps {
+  value: number;
+}
 
-const Progress = ({}: ProgressProps) => {
-  return <div></div>;
+const Progress = ({ value }: ProgressProps) => {
+  return (
+    <div className="w-full bg-neutral-quaternary rounded-full h-2">
+      <div
+        className="bg-brand h-2 rounded-full"
+        style={{ width: `${(value * 100).toString()}%` }}
+      ></div>
+    </div>
+  );
 };
 
 export default Progress;
